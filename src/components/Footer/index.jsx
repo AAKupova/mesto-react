e876cd49c6ed3b4d338b3608027a0499a3709595
Kupova Anastatia <aa.kupova@yandex.ru>
2021-10-05 20:13:26 +0300

    Переписала компонент Footer на styled-components!

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Text } from './Text';
 
-import './styles.css';
+export const Footer = (props) => (<StyledFooter {...props}><Text /></StyledFooter>);
 
-export const Footer = () => (
-  <footer className="footer">
-    <Text />
-  </footer>
-);
+const StyledFooter = styled.footer`
+  margin: 0 0 36px 0;
+  width: 282px;
+
+  @media ${(props) => props.device.device.tablet} {
+    margin: 0 0 36px 27px;
+  }
+
+  @media ${(props) => props.device.device.laptops} {
+    margin: 0 0 60px 0;
+  }
+`;
